fcb8fabe6308f86fe07cc541e2b63c86fb176737
mohanram <mohanraam2000@gmail.com>
2023-04-17 23:58:55 +0530

    FirstProjectStyleSheet toggle darkmode completed

--- firstProject/index.js
+++ firstProject/index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 
-import {App} from './App';
+import StyleSheetComponent from './Components/StyleSheetComponent';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => StyleSheetComponent);
