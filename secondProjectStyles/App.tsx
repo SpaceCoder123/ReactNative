92445dd9b69f32ccfd30319e7e9c3e03fada1de1
mohanram <mohanraam2000@gmail.com>
2023-04-18 23:35:12 +0530

    SideScrollView

--- secondProjectStyles/App.tsx
+++ secondProjectStyles/App.tsx
@@ -3,12 +3,14 @@
 import {SafeAreaView, ScrollView} from "react-native";
 import React from "react";
 import FlatCard from "./components/flatCard";
+import ElevatedCards from "./components/ElevatedCards";
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCard />
+        <ElevatedCards/>
       </ScrollView>
     </SafeAreaView>
   );
