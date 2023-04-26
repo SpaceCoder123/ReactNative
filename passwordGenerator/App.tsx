940d9b6b0e97b3e6feeacebf3244c81da977e1f6
mohanram <mohanraam2000@gmail.com>
2023-04-26 22:31:33 +0530

    passwordGenerator BusinessLogic

--- passwordGenerator/App.tsx
+++ passwordGenerator/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable comma-dangle */
 /* eslint-disable quotes */
@@ -24,9 +25,48 @@ const App = () => {
   const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
 
-  const generatePasswordString = (passwordLength: number) => {};
-  const createPassword = (characters: string, passwordLength: number) => {};
-  const resetPasswordState = (characters: string, passwordLength: number) => {};
+  const generatePasswordString = (passwordLength: number) => {
+    let characterList = '';
+
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '1234567890';
+    const specialChars = '!@#$%&*()_+';
+
+    if (upperCase) {
+      characterList += upperCase;
+    }
+    if (lowerCase) {
+      characterList += lowerCase;
+    }
+    if (numbers) {
+      characterList += numbers;
+    }
+    if (symbols) {
+      characterList += symbols;
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength);
+    setPassword(passwordResult);
+    setisPassGenerated(true);
+  };
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
+  const resetPasswordState = (characters: string, passwordLength: number) => {
+    setPassword('');
+    setisPassGenerated(false);
+    setLowerCase(false);
+    setUpperCase(false);
+    setNumbers(false);
+    setSymbols(false);
+  };
   return (
     <View>
       <Text>App</Text>
